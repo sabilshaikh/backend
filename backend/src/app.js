d5df74d19a6a2c28ebe11903573bbb67946bb8dc
sabil <sabilshaikh438@gmail.com>
2026-02-28 10:54:22 +0530

    Setup production build and serve React app from backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const noteModel = require("./model/note.model");
 const cors = require("cors")
-
+const path = require("path");
+const { log } = require("console");
 
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static("./public"))
 
 app.post("/api/notes", async (req , res) =>{
 
@@ -33,9 +35,51 @@ app.get("/api/notes", async (req, res)=>{
 })
 
 
+app.delete("/api/notes/:id", async (req , res) =>{
+
+  const noteId =  req.params.id;
+
+
+  
+
+  await noteModel.findByIdAndDelete(noteId);
+
+  
+
+   res.status(200).json({
+    message : "note successfully deleted"
+
+   })
+
+})  
+
+
+app.patch("/api/notes/:id", async (req , res) =>{
+  const id = req.params.id;
 
 
 
+
+ const newNote = await noteModel.findByIdAndUpdate(id, req.body);
+ console.log(newNote);
+ 
+
+  res.status(200).json({
+    message : "data updated successfully",
+    newNote
+
+  })
+
+
+})
+
+
+
+app.use("*name",(req , res) =>{
+  console.log(__dirname);
+  
+res.sendFile(path.join(__dirname,"..", "public", "index.html"))
+})
 
 
 module.exports = app;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 
@@ -10,6 +10,7 @@ const App = () => {
 ]
 )
 
+function fetchNotes (){
 axios.get("http://localhost:3000/api/notes").then((res) =>{
 
 setNotes(res.data.notes)
@@ -17,16 +18,76 @@ setNotes(res.data.notes)
 
 })
 
+}
+
+useEffect(()=>{
+
+  fetchNotes()
+
+},[])
+
+function submitHandler(e){
+  e.preventDefault();
+
+const {title , description} = e.target.elements;
+
+// console.log(title.value , description.value);
+
+axios.post("http://localhost:3000/api/notes", {
+  title : title.value,
+  description : description.value,
+}).then((res)=>{
+  // console.log(res.data);
+  fetchNotes()
+  
+  
+})
+
+}
+
+function deleteHandler(id){
+
+axios.delete("http://localhost:3000/api/notes/" + id)
+.then((res)=>{
+
+  fetchNotes()
+  
+})
+
+}
+
+// function updateHandler (id){
+
+//   axios.patch("http://localhost:3000/api/notes/"+id).then((res)=>{
+// console.log(res);
+
+//   })
+
+// }
+
+
   return (
    <>
+
+<form onSubmit={submitHandler}>
+
+<input type="text" name='title' placeholder='Enter title' />
+<input type="text" name='description' placeholder='enter description' />
+<button>Create note</button>
+
+</form>
+
 <div className="notes">
   
   {
-    notes.map((item) =>{
+    notes.map((item,idx) =>{
 
-      return <div className="note">
+      return <div key={idx} className="note">
     <h1>{item.title}</h1>
     <p>{item.description}</p>
+    <button onClick={()=>{
+      deleteHandler(item._id)
+    }}>delete</button>
   </div>
 
     })
